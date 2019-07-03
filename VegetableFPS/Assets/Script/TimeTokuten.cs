d1fc3d448782a5db3ea44cc2c278b6bfb9dfb170
Hikaru0416 <kbc17b26@stu.kawahara.ac.jp>
2019-07-03 13:37:33 +0900

    risalt

---
+++ VegetableFPS/Assets/Script/TimeTokuten.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeTokuten : MonoBehaviour
+{
+    float tokuten;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*float tokuten;
+        tokuten = timerScript.GetTimeTokuten();
+        Debug.Log(tokuten);
+        */
+        tokuten = Timer.GetCountTime();
+        print(tokuten);
+      
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Text>().text = tokuten.ToString("F2");
+    }
+
+}
--- VegetableFPS/Assets/Script/Timer.cs
+++ VegetableFPS/Assets/Script/Timer.cs
@@ -5,17 +5,38 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour
 {
 
-    float countTime = 0;
-    // Use this for initialization
-    void Start()
+    public static float countTime = 300;
+  //public float Timetokuten;
+        void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        countTime += Time.deltaTime; //スタートしてからの秒数を格納
+        countTime -= Time.deltaTime; //スタートしてからの秒数を格納
         GetComponent<Text>().text = countTime.ToString("F2"); //小数2桁にして表示
+
+        /*void OnCollisionEnter(Collision collision)
+            {
+                if (collision.gameObject.name == "Goal")
+                {
+                    Timetokuten = countTime;
+
+                }
+
+            }
+            */
+
     }
+    public static float GetCountTime()
+    {
+        return countTime;
+    }
+    /*public float GetTimeTokuten()
+    {
+        return Timetokuten;
+    }*/
+
 }
