f335d81b5da5bc0dc817032d07791d6483552ddf
TrilobiteFutaba <kbc17b08@stu.kawahara.ac.jp>
2019-07-04 14:35:45 +0900

    Sound追加

---
+++ VegetableFPS/Assets/Script/Target.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private int ClickCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ClickCount = 0;
+
+    }
+
+    void OnTriggerEnter(Collider Bullet)
+    {
+        ClickCount++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if (ClickCount >= 3)
+        {
+            this.gameObject.transform.Translate(0, 0.2f, 0);
+        }
+    }
+}
