17f0d08b724939a327cb9fc06371a532f51c598d
TrilobiteFutaba <kbc17b08@stu.kawahara.ac.jp>
2019-07-11 12:09:44 +0900

    BGM追加？

---
+++ VegetableFPS/Assets/Script/Killtokuten.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Killtokuten : MonoBehaviour
+{
+    float killtokuten;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //killtokuten = HP.GetKill();
+        print(killtokuten);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Text>().text = killtokuten.ToString("F2");
+    }
+}
